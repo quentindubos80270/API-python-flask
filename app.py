ec034edb87f34551533c87f040a8769e25042e24
quentindubos80270 <quentin.dubos80270@gmail.com>
2026-04-19 19:21:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ items = []
 
 @app.route("/")
 def home():
-    return jsonify({"message": "Hello World"})
+    return jsonify({"message": "Hello World!"})
 
 @app.route("/items", methods=["GET"])
 def get_items():
