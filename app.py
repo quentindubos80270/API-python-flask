2740128293d8d306c869f3ed58035e705069758a
quentindubos80270 <quentin.dubos80270@gmail.com>
2026-04-19 19:14:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,17 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-#Fake database
 items = []
 
-#Définition des routes
 @app.route("/")
 def home():
     return jsonify({"status": "Hello World"})
 
-@app.route("/items",methods=["GET"])
+@app.route("/items", methods=["GET"])
 def get_items():
     return jsonify(items)
 
-@app.route("/items",methods=["POST"])
+@app.route("/items", methods=["POST"])
 def create_items():
     data = request.get_json()
 
