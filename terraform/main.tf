bb61bde52ce433fd55aa7508bcbd46e871e2fea0
quentindubos80270 <quentin.dubos80270@gmail.com>
2026-05-02 16:36:16 +0200

    Provide render

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,39 +2,51 @@ terraform {
   required_providers {
     render = {
       source  = "render-oss/render"
-      version = "~> 1.0"
+      version = "1.7.5"
     }
   }
+}
+
+variable "api_key" {
+  type = string
+}
+
+variable "owner_id" {
+  type = string
+}
+
+variable "environment" {
+  type = string
 }
 
 provider "render" {
-  api_key = var.render_api_key
+  api_key = var.api_key
+  owner_id = var.owner_id
 }
 
-variable "render_api_key" {
-  type      = string
-  sensitive = true
-}
-
-resource "render_web_service" "flask_api" {
-  name   = "api-python-flask"
-  region = "frankfurt"
-
-  runtime_source = {
-    type = "docker"
-    image = {
-      url = "ghcr.io/quentindubos80270/api-python-flask/api-iscod:latest"
+resource "render_project" "api-python-flask" {
+  name = "api-python-flask"
+  environments = {
+    "production" = {
+      name = "production"
+      protected_status = "unprotected"
+    },
+    "development" = {
+      name = "development"
+      protected_status = "unprotected"
     }
   }
+}
 
-  plan = "free"
-
-  env_vars = [
-    {
-      key   = "ENV"
-      value = "production"
+resource "render_web_service" "api-python-flask-app" {
+  name = "api-iscod"
+  plan = "starter"
+  region = "frankfurt"
+  environment_id = render_project.api-python-flask.environments[var.environment].id
+  runtime_source = {
+    image = {
+      image_url = "ghcr.io/quentindubos80270/api-python-flask/api-iscod"
+      tag = "latest"
     }
-  ]
-
-  health_check_path = "/health"
+  }
 }
---
+++ terraform/render.tfvars
@@ -0,0 +1,3 @@
+api_key="rnd_hSQLjxB3m5gfijl1z7LjsBpec01d"
+owner_id="tea-d7f4rdvlk1mc73c6c5tg"
+environment="development"
